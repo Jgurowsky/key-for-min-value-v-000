ce6a5554df31fe77812cc9075782745d9803c7c0
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-13 21:02:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
   array = name_hash.collect{|key, value|}
+  array
 end
 
 key_for_min_value(name_hash)
