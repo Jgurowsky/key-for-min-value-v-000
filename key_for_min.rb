957f732a15a1b4bed45fb7443b2cf05287360f78
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-13 21:22:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,10 @@ require 'pry'
 
 
 
-name_hash = {:chair => 25, :table => 85, :mattress => 450}
-
 def key_for_min_value(name_hash)
   arr = name_hash.collect {|key, value| value}.sort
+  binding pry
 end
-binding pry
+
 
 key_for_min_value(name_hash)
