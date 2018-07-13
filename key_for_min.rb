0525d5e50c365bf6cc11a53f4ad515494792363f
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-13 21:11:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 name_hash = {:chair => 25, :table => 85, :mattress => 450}
@@ -5,6 +7,7 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   array = name_hash.collect{|key, value| value}
   array.sort
+  binding pry
 end
 
 #key_for_min_value(name_hash)
