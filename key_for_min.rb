8f8fec9d793d2932b5d8a93693111dd4876f3c6e
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-13 21:10:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
   array = name_hash.collect{|key, value| value}
-  array
+  array.sort
 end
 
 key_for_min_value(name_hash)
